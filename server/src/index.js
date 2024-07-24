0b6e91b6f15d8261fd62f3d4bd3144912ff2fc9b
sdevmarc <suanieeee@yahoo.com>
2024-07-24 22:55:57 +0800

    chore: Remove unused HOST variable in server index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 const app = require('./App')
 
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.devHOST;
+// const HOST = process.env.devHOST;
 
-app.listen(PORT, HOST, () => {
-    console.log(`Listening: http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Connected to server`);
 });
 
