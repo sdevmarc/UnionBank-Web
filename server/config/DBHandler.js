3a85b5cbcada067809c7a53626c3d746cd404f36
sdevmarc <suanieeee@yahoo.com>
2024-07-24 23:00:57 +0800

    chore: Update server configuration to use environment variables for MongoDB connection

--- server/config/DBHandler.js
+++ server/config/DBHandler.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDb = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/unionbank')
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log('Connected to the database!')
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
