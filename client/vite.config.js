24444333001b7e368c09c39efb0aa6847576068e
Marc Edison <suanieeee@yahoo.com>
2024-12-11 01:38:11 +0800

    Add Tailwind CSS support in Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from "path"
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [react({
         jsxRuntime: 'automatic',
         fastRefresh: true
     })],
+    css: {
+        postcss: {
+            plugins: [tailwindcss()],
+        },
+    },
     build: {
         outDir: 'dist', // Default output directory
         // sourcemap: true, // Ensure sourcemaps are generated
