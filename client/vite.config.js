6162a411b08babb21a7023f845599a3cf86e69da
Marc Edison <suanieeee@yahoo.com>
2024-12-11 01:15:05 +0800

    Add sourcemap configuration for non-production builds in Vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        sourcemap: process.env.NODE_ENV !== 'production',
+    },
     server: {
         // host: '192.168.10.14',
         // port: '80'
